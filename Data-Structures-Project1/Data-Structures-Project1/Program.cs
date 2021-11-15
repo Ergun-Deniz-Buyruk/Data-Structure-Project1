9b76ad8041f97b954d4df329352781469f9fbf27
Ergun-Deniz-Buyruk <ergunndenizbuyruk@gmail.com>
2021-11-15 23:57:20 +0300

    proje yeniden olusturuldu.

--- Data-Structures-Project1/Data-Structures-Project1/Program.cs
+++ Data-Structures-Project1/Data-Structures-Project1/Program.cs
@@ -1,23 +1,24 @@
 ﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Collections;
 
-namespace ConsoleApp7
+namespace Data_Structures_Project1
 {
-
     class Program
     {
+
         static Random rastgele = new Random();
         public static double[,] rastgele_nokta(int n_par, int genişlik_par, int yükseklik_par)
         {
             double[,] a = new double[n_par, 2];
-            for (int i =0; i< n_par; i++)
+            for (int i = 0; i < n_par; i++)
             {
-                double genişlik = rastgele.NextDouble()*100;
-                double yükseklik = rastgele.NextDouble()*100;
+                double genişlik = rastgele.NextDouble() * 100;
+                double yükseklik = rastgele.NextDouble() * 100;
 
                 //
 
@@ -25,42 +26,42 @@ namespace ConsoleApp7
                 a[i, 1] = yükseklik;
             }
             return a;
-        }  
+        }
 
         public static void dolaşım(double[,] liste1_par, double[,] liste_n_x_n_par)
         {
-            for (int tur_numarası =0; tur_numarası< 10; tur_numarası++)
+            for (int tur_numarası = 0; tur_numarası < 10; tur_numarası++)
             {
                 ArrayList sıra_listesi = new ArrayList();
                 double yol_uzunluğu = 0;
-                
+
                 double[,] kopya_liste = new double[liste1_par.GetLength(0), 2];
-                for(int len=0; len<liste1_par.GetLength(0); len++)
+                for (int len = 0; len < liste1_par.GetLength(0); len++)
                 {
                     kopya_liste[len, 0] = liste1_par[len, 0];
                     kopya_liste[len, 1] = liste1_par[len, 1];
                 }
                 int başlangıç_noktası = rastgele.Next(20);
-                
+
                 sıra_listesi.Add(başlangıç_noktası);
                 int bir_sonraki_nokta = 50;
-                
-                
-                
-                
+
+
+
+
                 double min_uzaklık;
                 if (başlangıç_noktası != 0)
                 {
                     min_uzaklık = liste_n_x_n_par[başlangıç_noktası, başlangıç_noktası - 1];
-                   
+
                 }
                 else
                 {
                     min_uzaklık = liste_n_x_n_par[başlangıç_noktası, başlangıç_noktası + 1];
-                    
+
                 }
 
-                
+
                 for (int y = 0; y < 20; y++)
                 {
                     if ((liste_n_x_n_par[başlangıç_noktası, y] < min_uzaklık) && (liste_n_x_n_par[başlangıç_noktası, y] != 0))
@@ -68,7 +69,7 @@ namespace ConsoleApp7
                         min_uzaklık = liste_n_x_n_par[başlangıç_noktası, y];
                         bir_sonraki_nokta = y;
                     }
-                    
+
 
                 }
                 yol_uzunluğu += min_uzaklık;
@@ -89,7 +90,7 @@ namespace ConsoleApp7
                     }
                     for (int ü = 0; ü < 20; ü++)
                     {
-                        if ((liste_n_x_n_par[bir_sonraki_nokta, ü] < min_uzaklık_2) && (liste_n_x_n_par[bir_sonraki_nokta, ü] != 0)&& sıra_listesi.Contains(bir_sonraki_nokta)== false)
+                        if ((liste_n_x_n_par[bir_sonraki_nokta, ü] < min_uzaklık_2) && (liste_n_x_n_par[bir_sonraki_nokta, ü] != 0) && sıra_listesi.Contains(bir_sonraki_nokta) == false)
                         {
                             min_uzaklık_2 = liste_n_x_n_par[başlangıç_noktası, ü];
                             bir_sonraki_nokta = ü;
@@ -103,7 +104,7 @@ namespace ConsoleApp7
 
                 Console.WriteLine("tur numarası: " + tur_numarası);
                 Console.WriteLine("uzunluk: " + yol_uzunluğu);
-                for(int n =0; n<20; n++)
+                for (int n = 0; n < 20; n++)
                 {
                     Console.Write(sıra_listesi[n] + "  ");
 
@@ -114,17 +115,32 @@ namespace ConsoleApp7
             }
         }
 
-        static void Main(string[] args)
+        /* 
+         * Kendisine verilen noktalar matrisini alarak her nokta arasindaki 
+         * uzakligi bulup n*n lik uzaklik matrisini uretip bu matrisi donduren metot. 
+         */
+        public static double[,] uzaklikMatrisi(double[,] noktalarMatrisi)
         {
-            
-            double[,] listem = rastgele_nokta(20,100,100); 
+            // Gelen matristeki toplam nokta sayisini bul.
+            private int noktaSayisi = noktalarMatrisi.Length();
 
-            
-            Console.ReadLine();
+        // Once uzaklik matrisini uretelim.
+        private double[noktaSayisi, noktaSayisi] uzaklikMatrisi = new double[noktaSayisi, noktaSayisi];
 
-            
 
-            
-        }
+
+
     }
+
+    public static void Main(string[] args)
+    {
+
+        double[,] listem = rastgele_nokta(20, 100, 100);
+
+
+        Console.ReadLine();
+
+
+    }
+    
 }
